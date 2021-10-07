2f8a463be7eb110e440ba67a35397993d4160cd9
Anthony Patton <anthonyalanpatton@gmail.com>
2021-10-06 23:43:59 -0700

    Added 2 challenges

--- 1-Medium/5-MaxSum.java
+++ 1-Medium/5-MaxSum.java
@@ -1,2 +1,3 @@
 // Given an array of positive numbers, find the maximum sum subsequence such that elements in this subsequence are not adjacent to each other. 
 
+// Still working on this one, not sure of the results yet. 
