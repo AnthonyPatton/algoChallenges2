8534ade74f414d5f78fa03f4b0fb541d86e6e829
Anthony Patton <anthonyalanpatton@gmail.com>
2021-10-13 08:10:35 -0700

    Added edits

--- 1-Medium/8-CoinChange2.java
+++ 1-Medium/8-CoinChange2.java
@@ -1,2 +1,20 @@
 // // Total Unique Ways to Make Change
 // Its a dynamic programming question
+
+// You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+// Return the number of combinations that make up that amount. If that amount of money cannot be made up by any combination of the coins, return 0.
+
+class Solution {
+  public int change(int amount, int[] coins) {
+    int[] dp = new int[amount + 1];
+    dp[0] = 1;
+
+    for (int coin : coins) {
+      for (int x = coin; x < amount + 1; ++x) {
+        dp[x] += dp[x - coin];
+      }
+    }
+    return dp[amount];
+  }
+}
