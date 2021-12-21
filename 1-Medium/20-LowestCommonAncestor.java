3a14344313fd7184b02e1d582070a6a4b62a8cdc
Anthony Patton <anthonyalanpatton@gmail.com>
2021-12-21 00:03:55 -0800

    Adding challenge

--- 1-Medium/20-LowestCommonAncestor.java
+++ 1-Medium/20-LowestCommonAncestor.java
@@ -1,1 +1,12 @@
-// Adding challenge
+// Given two nodes in a binary tree, write a function to find the lowest common ancestor.
+
+/*      1
+      /   \
+    2       3
+   / \     / \
+  4   5   6   7
+  
+  lcs(4, 3) = 1
+  lcs(6, 7) = 3
+  
+  */
