9328c107111e07cb2b4f2ef88a0dbe257d3369a5
Anthony Patton <anthonyalanpatton@gmail.com>
2021-12-23 23:34:43 -0800

    Working on solution

--- 1-Medium/20-LowestCommonAncestor.java
+++ 1-Medium/20-LowestCommonAncestor.java
@@ -10,3 +10,33 @@
   lcs(6, 7) = 3
   
   */
+
+  private class Node {
+    private Node left;
+    private Node right;
+  }
+  0(n)
+
+  public Node lca(Node tree, Node n1, Node n2) {
+    if (n1.equals(n2)) return n1;
+
+    Stack pathToN1 = pathTo(tree, n1);
+    Stack pathToN2 = pathTo(tree, n2);
+    if (pathToN1 == null || pathToN2 == null) return null;
+
+    Node prev = null;
+    while (!pathToN1.isEmpty() && !pathToN2.isEmpty()) {
+      Node s = pathToN1.pop();
+      Node t = pathToN2.pop();
+      if (s.equals(t)) prev = s;
+      else break;
+    }
+    return prev;
+  }
+
+  public Stack<Node> pathTo(Node tree, Node n) {
+    if (tree == null) return null;
+    if (tree.equals(n)) {
+      Stack<Node> s = new Stack<Node>();
+    }
+  }
